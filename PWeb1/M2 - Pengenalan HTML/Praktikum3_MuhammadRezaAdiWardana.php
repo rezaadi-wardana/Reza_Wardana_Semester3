ae4e3234167404c6c809aa6be8931ceb218ebded
rezaadi-wardana <wardhana15.aw@gmail.com>
2023-10-07 12:59:11 +0700

    Tugas modul 2

---
+++ PWeb1/M2 - Pengenalan HTML/Praktikum3_MuhammadRezaAdiWardana.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 3</title>
+</head>
+<body>
+
+<h1 align="center" >WEBSITE PROTOFOLIO REZA</h1>
+<h2 style="color:blue;">Halo... Saya Muhammad Reza Adi Wardana</h2>
+<p style="color:red;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, omnis doloremque ad voluptatibus dolores impedit sint libero! Corrupti, cupiditate. Esse corrupti eius velit? Assumenda, corrupti ducimus dolor iure aliquam numquam.</p>
+    
+</body>
+</html>

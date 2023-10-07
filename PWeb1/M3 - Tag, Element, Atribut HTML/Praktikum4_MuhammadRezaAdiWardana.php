5616f88805ae1a39455cc4ab8674aa06d57bc2ef
rezaadi-wardana <wardhana15.aw@gmail.com>
2023-10-08 05:11:55 +0700

    modul 3

---
+++ PWeb1/M3 - Tag, Element, Atribut HTML/Praktikum4_MuhammadRezaAdiWardana.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+    </HEAD>
+    <FRAMESET COLS="25%,75%">
+        <frame src="nama.html">
+        <FRAMESET ROWS="15%,75%">
+        <frame name="header" src="header.html">
+        <frame name="info" src="infoReza.html">
+    </FRAMESET>
+</FRAMESET>
+</HTML>

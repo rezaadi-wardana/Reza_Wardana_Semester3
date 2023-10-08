6c3002edef6e98de8595963011d57b8a1916b847
rezaadi-wardana <wardhana15.aw@gmail.com>
2023-10-08 09:08:15 +0700

    tugas

---
+++ PWeb1/M3 - Tag, Element, Atribut HTML/Praktikum5_MuhammadRezaAdiWardana.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <h1>FORM HTML</h1>
+
+    <label for="nama">Name :</label>
+    <input type="text" id="nama"><br>
+
+    <label for="">Gender :</label>
+    <input type="radio" id="man" >
+    <label for="man">Man</label>
+    <input type="radio" id="woman">
+    <label for="woman">Woman</label><br>
+
+    <label for="address">Address</label>
+    <input type="text"id="address"><br>
+
+    <label for="country">Country</label>
+    <select name="country" id="country">
+        <option value="1">Indonesia</option>
+        <option value="2">Malaysia</option>
+        <option value="3">Singapore</option>
+        <option value="4">Timor Leste</option>
+        <option value="5">Brunei</option>
+        <option value="6">Australia</option>
+    </select><br>
+
+    <label for="email">Email :</label>
+    <input type="text" id="email"><br>
+
+    <label for="Photo">Photo :</label>
+    <input type="image">
+</body>
+</html>

1ef06570fb1c3923ab999aff0369b6f639895b4c
rezaadi-wardana <wardhana15.aw@gmail.com>
2023-10-26 12:46:53 +0700

    update

--- M4 - Website HTML/sidebar/sidebar.php
+++ M4 - Website HTML/sidebar/sidebar.php
@@ -1,9 +1,13 @@
 <HTML>
 <HEAD>
     </HEAD>
-    <FRAMESET COLS="30%,70%">
-        <frame src="nama.html">
-        <frame name="info" src="infoReza.html">
-    </FRAMESET>
-</FRAMESET>
+    <frameset rows="10%, 90%">
+            <frame src="../navbar.html" name="navbar" >
+        <FRAMESET COLS="70%,30%">
+            <frame name="info" src="../mainbar.html" style="background-color:rgb(255, 199, 95); padding-left: 2em;">
+                <frame src="nama.html">
+        </FRAMESET>
+    
+
+ </frameset>
 </HTML>
